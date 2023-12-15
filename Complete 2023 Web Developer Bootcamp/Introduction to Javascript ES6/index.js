4de9c29caa031ca1d10ba589cc8895a5fadb1edc
Sami Munir <sambreezy2001@gmail.com>
2023-12-15 18:52:07 -0500

    learning JS properties

--- Complete 2023 Web Developer Bootcamp/Introduction to Javascript ES6/index.js
+++ Complete 2023 Web Developer Bootcamp/Introduction to Javascript ES6/index.js
@@ -34,3 +34,13 @@ swapVals(2, 3);
 my_name = 'Sami Munir';
 console.log('\nmy_name: ' + my_name)
 console.log('my_name.length: ' + my_name.length)
+console.log('my_name.slice(0, 4): ' + my_name.slice(0, 4));
+
+function sayHello(name) {
+    var first_char = name.slice(0, 1);
+    var uppercase_first_char = first_char.toUpperCase();
+    var remaining_name = name.slice(1, name.length);
+    var greeting_name = uppercase_first_char + remaining_name;
+    console.log('\nHello, ' + greeting_name);
+}
+sayHello('sami');
