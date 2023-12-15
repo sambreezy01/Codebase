39dd05c19e9860f9b91fc34c4bdfe28536b60445
Sami Munir <sambreezy2001@gmail.com>
2023-12-15 04:40:37 -0500

    learning JS data types & variables

---
+++ Complete 2023 Web Developer Bootcamp/Introduction to Javascript ES6/index.js
@@ -0,0 +1,10 @@
+/*
+    Complete 2023 Web Developer Bootcamp
+    - Introduction to Javascript ES6
+*/
+console.log('Hello world!');
+// alert('Hello world! (alert)');
+// prompt('What is your name? (prompt)')
+
+var my_name = 'Sami';
+console.log('\nmy_name: ' + my_name);
