5ef3447570943b12c2af4e8a86a9c132cf85275e
Sami Munir <sambreezy2001@gmail.com>
2023-12-15 02:29:55 -0500

    adding function get_weather_data()

--- Computer Science Projects/Simple Weather App/main.py
+++ Computer Science Projects/Simple Weather App/main.py
@@ -6,3 +6,16 @@
 # Python, requests, openweathermap API
 
 import requests
+
+def get_weather_data(location):
+    API_KEY = 'ff1672a76779bb7575d444b1cd67f2ff'
+    BASE_URL = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}'
+    response = requests.get(BASE_URL)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print('Failed to fetch weather data...')
+        return None
+
+# next function goes here...
