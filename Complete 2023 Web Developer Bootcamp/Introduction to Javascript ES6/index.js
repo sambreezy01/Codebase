ec3015af2d5ee331184751a095486b773300e872
Sami Munir <sambreezy2001@gmail.com>
2023-12-15 18:23:53 -0500

    learning JS strings

--- Complete 2023 Web Developer Bootcamp/Introduction to Javascript ES6/index.js
+++ Complete 2023 Web Developer Bootcamp/Introduction to Javascript ES6/index.js
@@ -8,3 +8,29 @@ console.log('Hello world!');
 
 var my_name = 'Sami';
 console.log('\nmy_name: ' + my_name);
+my_name = 'John';
+console.log('my_name (updated): ' + my_name);
+
+a = 2;
+b = 3;
+function getSum(a, b) {
+    return a + b;
+}
+console.log('\na: ' + a);
+console.log('b: ' + b);
+console.log('a + b (sum): ' + getSum(a, b));
+
+function swapVals(a, b) {
+    console.log('\na (original): ' + a);
+    console.log('b (original): ' + b);
+    var temp = a;
+    a = b;
+    b = temp;
+    console.log('a (swapped): ' + a);
+    console.log('b (swapped): ' + b);
+}
+swapVals(2, 3);
+
+my_name = 'Sami Munir';
+console.log('\nmy_name: ' + my_name)
+console.log('my_name.length: ' + my_name.length)
