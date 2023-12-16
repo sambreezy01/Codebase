e6b7fe9071df4875ae9f4113ca3aa9ec945e6dbb
Sami Munir <sambreezy2001@gmail.com>
2023-12-15 22:48:12 -0500

    learning DOM manipulation using getElementsBy..

--- Complete 2023 Web Developer Bootcamp/The Document Object Model (DOM)/index.js
+++ Complete 2023 Web Developer Bootcamp/The Document Object Model (DOM)/index.js
@@ -1,1 +1,7 @@
-alert('Hello, Sami!');
+// document.querySelector('h1').style.color = 'red';
+
+// document.querySelector('input').click();
+
+document.getElementsByTagName('li')[2].style.color = 'blue';
+document.getElementsByClassName('btn')[0].style.color = 'blue';
+document.getElementById('title').style.color = 'blue';
