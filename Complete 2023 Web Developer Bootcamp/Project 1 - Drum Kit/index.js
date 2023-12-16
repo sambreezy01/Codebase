8ee172928ba4689c0711a71db8ff81701ce03725
Sami Munir <sambreezy2001@gmail.com>
2023-12-16 02:55:50 -0500

    Javascript high-order functions & passing functions as arguments

--- Complete 2023 Web Developer Bootcamp/Project 1 - Drum Kit/index.js
+++ Complete 2023 Web Developer Bootcamp/Project 1 - Drum Kit/index.js
@@ -14,3 +14,16 @@ for (let i = 0; i < buttons_set.length; i++) {
         alert('I got clicked.');
     });
 }
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function calculator(num1, num2, operator) {
+    return operator(num1, num2);
+}
+calculator(2, 3, add);
