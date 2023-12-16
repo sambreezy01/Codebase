132ad5039fc3f6fa9dd7e2775aedc7be891866c7
Sami Munir <sambreezy2001@gmail.com>
2023-12-16 01:16:43 -0500

    manipulating CSS with Javascript

--- Complete 2023 Web Developer Bootcamp/The Document Object Model (DOM)/index.js
+++ Complete 2023 Web Developer Bootcamp/The Document Object Model (DOM)/index.js
@@ -2,6 +2,14 @@
 
 // document.querySelector('input').click();
 
-document.getElementsByTagName('li')[2].style.color = 'blue';
-document.getElementsByClassName('btn')[0].style.color = 'blue';
-document.getElementById('title').style.color = 'blue';
+// document.getElementsByTagName('li')[2].style.color = 'blue';
+// document.getElementsByClassName('btn')[0].style.color = 'blue';
+// document.getElementById('title').style.color = 'blue';
+// document.querySelector('#list').style.listStyleType = 'none';
+// document.querySelector('li a').style.color = 'red';
+
+document.querySelector('h1').style.color = 'red';
+
+document.getElementsByClassName('btn')[0].style.backgroundColor = 'yellow';
+document.getElementsByClassName('btn')[0].style.outline = 'none';
+document.getElementsByClassName('btn')[0].style.border = 'none';
