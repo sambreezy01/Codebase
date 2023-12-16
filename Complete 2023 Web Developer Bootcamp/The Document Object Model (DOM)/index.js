a8c08a489506385b4ddd5de15053ddf5f72eb61d
Sami Munir <sambreezy2001@gmail.com>
2023-12-16 01:29:34 -0500

    Javascript DOM manipulation classList and set/getAttributes()

--- Complete 2023 Web Developer Bootcamp/The Document Object Model (DOM)/index.js
+++ Complete 2023 Web Developer Bootcamp/The Document Object Model (DOM)/index.js
@@ -8,8 +8,13 @@
 // document.querySelector('#list').style.listStyleType = 'none';
 // document.querySelector('li a').style.color = 'red';
 
-document.querySelector('h1').style.color = 'red';
+// document.querySelector('h1').style.color = 'red';
 
-document.getElementsByClassName('btn')[0].style.backgroundColor = 'yellow';
-document.getElementsByClassName('btn')[0].style.outline = 'none';
-document.getElementsByClassName('btn')[0].style.border = 'none';
+// document.getElementsByClassName('btn')[0].style.backgroundColor = 'yellow';
+// document.getElementsByClassName('btn')[0].style.outline = 'none';
+// document.getElementsByClassName('btn')[0].style.border = 'none';
+
+// document.querySelector('.btn').classList.remove('.invisible');
+
+document.querySelector('h1').classList.add('huge');
+document.querySelector('a').setAttribute('href', 'https://www.bing.com');
