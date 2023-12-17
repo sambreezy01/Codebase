97e0bcca6169624f5364b8f4930ec361e9497a3f
Sami Munir <sambreezy2001@gmail.com>
2023-12-16 22:53:58 -0500

    adding audio functionality javascript

--- Complete 2023 Web Developer Bootcamp/Project 1 - Drum Kit/index.js
+++ Complete 2023 Web Developer Bootcamp/Project 1 - Drum Kit/index.js
@@ -8,22 +8,25 @@
 //     alert('I got clicked.');
 // });
 
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function calculator(num1, num2, operator) {
+//     return operator(num1, num2);
+// }
+// calculator(2, 3, add);
+
 var buttons_set = document.querySelectorAll('.drum');
 for (let i = 0; i < buttons_set.length; i++) {
     buttons_set[i].addEventListener('click', function() {
         alert('I got clicked.');
+        
+        var audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
     });
 }
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function calculator(num1, num2, operator) {
-    return operator(num1, num2);
-}
-calculator(2, 3, add);
